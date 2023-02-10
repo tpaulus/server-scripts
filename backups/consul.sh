2ceb2196471777e833df5370f310b582eb551bd4
Tom Paulus <tom@tompaulus.com>
2023-02-09 21:25:16 -0800

    Update scripts to run directly on hosts

--- backups/consul.sh
+++ backups/consul.sh
@@ -2,13 +2,20 @@
 
 set -e
 
-backup_file=$(mktemp)
+cluster_leader=`curl --silent --fail http://127.0.0.1:8500/v1/operator/raft/configuration | jq -r '.Servers[] | select(.Leader) | .Node'`
+echo "Cluster Leader: $cluster_leader"
 
-echo "Executing Consul Snapshot"
-consul snapshot save $backup_file
+if [[ `hostname` == $cluster_leader ]]; then
+    backup_file=$(mktemp)
 
-echo "Uploading Snapshot to NAS"
-rsync $backup_file rsync://woodlandpark.brickyard.whitestar.systems:873/raft-backups/consul.snap
+    echo "Executing Consul Snapshot"
+    consul snapshot save $backup_file
 
-rm $backup_file
+    echo "Uploading Snapshot to NAS"
+    rsync $backup_file rsync://woodlandpark.brickyard.whitestar.systems:873/raft-backups/consul.snap
+
+    rm $backup_file
 echo "Done!"
+else
+    echo "Skipping backup as $cluster_leader is the leader"
+fi
--- backups/nomad.sh
+++ backups/nomad.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
-set -e
+set -ex
 
-backup_file=$(mktemp)
+cluster_leader=`curl --silent --fail http://127.0.0.1:4646/v1/operator/raft/configuration | jq -r '.Servers[] | select(.Leader) | .Node' | sed -nr "s/([a-zA-Z0-9]+).[a-zA-Z0-9]+/\1/p"`
+echo "Cluster Leader: $cluster_leader"
 
-echo "Executing Nomad Snapshot"
-nomad snapshot save $backup_file
+if [[ `hostname` =~ $cluster_leader ]]; then
+    backup_file=$(mktemp)
 
-echo "Uploading Snapshot to NAS"
-rsync $backup_file rsync://woodlandpark.brickyard.whitestar.systems:873/raft-backups/nomad.snap
+    echo "Executing Nomad Snapshot"
+    nomad operator snapshot save $backup_file
 
-rm $backup_file
+    echo "Uploading Snapshot to NAS"
+    rsync $backup_file rsync://woodlandpark.brickyard.whitestar.systems:873/raft-backups/nomad.snap
+
+    rm $backup_file
 echo "Done!"
+else
+    echo "Skipping backup as $cluster_leader is the leader"
+fi
