810e84652512a0f0fb423daf4e83de048bd83542
Tom Paulus <tom@tompaulus.com>
2023-02-09 21:29:36 -0800

    Disable Step Logging

--- backups/nomad.sh
+++ backups/nomad.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-set -ex
+set -e
 
 cluster_leader=`curl --silent --fail http://127.0.0.1:4646/v1/operator/raft/configuration | jq -r '.Servers[] | select(.Leader) | .Node' | sed -nr "s/([a-zA-Z0-9]+).[a-zA-Z0-9]+/\1/p"`
 echo "Cluster Leader: $cluster_leader"
 
 if [[ `hostname` =~ $cluster_leader ]]; then
-    backup_file=$(mktemp)
+    backup_file="/tmp/nomad-$(date +%s).snap"
 
     echo "Executing Nomad Snapshot"
     nomad operator snapshot save $backup_file
